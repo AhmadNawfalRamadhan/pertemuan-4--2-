87e9d5a9e93b31b5bf62fb0df9a93acf85e70da3
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-04-20 15:58:14 +0700

    struct_g 3

--- struct_getline.cpp
+++ struct_getline.cpp
@@ -32,4 +32,16 @@ int main()
         cin >> mhs[i].alamat.provinsi;
         cin.ignore();
     }
+
+    cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl;
+    for (int i = 0 ; i < 2; i++)
+    {
+        cout << "Mahasisawa ke-" << i + 1 << endl;
+        cout << "NIM = " << mhs[i].nim << endl;
+        cout << "Nama = " << mhs[i].nama << endl;
+        cout << "kota = " << mhs[i].alamat.kota << endl;
+        cout << "Provinsi = " << mhs[i].alamat.provinsi << endl;
+    }
 }
