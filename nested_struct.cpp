847de1396e355cc7d7745b916b43822f10c980ad
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-04-20 15:10:05 +0700

    nested_s 1

---
+++ nested_struct.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+    string kota;
+    string provinsi;
+};
+
+struct mahasiswa
+{
+    string nama;
+    string nim;
+    DetailAlamat alamat;
+};
